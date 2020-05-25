343d629c9bf23971723d30b70f891121ac708cb7
Sundae <948820549@qq.com>
2020-05-25 23:10:20 +0800

    update

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,60 +1,41 @@
 <template>
     <div>
-        <el-header height="42px">
+        <el-header height="">
             <span>表单生成器</span>
+            <el-button @click="mainDrawer=true" style="float:right;margin-top: 8px;">打开Drawer</el-button>
         </el-header>
         <el-main>
             <div class="left-board">
                 <el-scrollbar>
-
+                    <LeftBoard/>
                 </el-scrollbar>
             </div>
             <div class="center-board">
                 <el-scrollbar>
-
                 </el-scrollbar>
             </div>
             <div class="right-board">
                 <el-scrollbar>
-
                 </el-scrollbar>
             </div>
-            <!--            <div class="left-board">-->
-            <!--                <el-srollbar class="left-scrollbar">-->
-            <!--                    <LeftBoard/>-->
-            <!--&lt;!&ndash;                    <draggable&ndash;&gt;-->
-            <!--&lt;!&ndash;                            :list="componentList"&ndash;&gt;-->
-            <!--&lt;!&ndash;                            :animation="340"&ndash;&gt;-->
-            <!--&lt;!&ndash;                            :group="{ name: 'testgroup', pull: 'clone', put: false }"&ndash;&gt;-->
-            <!--&lt;!&ndash;                            :sort="false"&ndash;&gt;-->
-            <!--&lt;!&ndash;                            :clone="onCloneHandler"&ndash;&gt;-->
-            <!--&lt;!&ndash;                    >&ndash;&gt;-->
-            <!--&lt;!&ndash;                        <div&ndash;&gt;-->
-            <!--&lt;!&ndash;                                class="component-item"&ndash;&gt;-->
-            <!--&lt;!&ndash;                                v-for="item in componentList"&ndash;&gt;-->
-            <!--&lt;!&ndash;                                :key="item.key"&ndash;&gt;-->
-            <!--&lt;!&ndash;                        >{{item.label}}&ndash;&gt;-->
-            <!--&lt;!&ndash;                        </div>&ndash;&gt;-->
-            <!--&lt;!&ndash;                    </draggable>&ndash;&gt;-->
-            <!--                </el-srollbar>-->
-            <!--            </div>-->
-            <!--            <div class="center-board">-->
-            <!--                <div class="center-scrollbar">-->
-            <!--                    <Main @onSelectComponent="onSelectFormComponent"/>-->
-            <!--                </div>-->
-            <!--            </div>-->
-            <!--            <div class="right-board">-->
-            <!--                <component :is="configView" v-model="config"></component>-->
-            <!--            </div>-->
         </el-main>
+        <el-drawer :visible.sync="mainDrawer" size="100%"></el-drawer>
     </div>
 </template>
 
 <script>
-    // import LeftBoard from "@/views/LeftBoard";
+    import LeftBoard from "@/views/LeftBoard";
+
     export default {
         name: "Index",
-        // components: {LeftBoard},
+        components: {
+            LeftBoard
+        },
+        data() {
+            return {
+                mainDrawer: false,
+            }
+        }
     }
 </script>
 
--- src/views/LeftBoard.vue
+++ src/views/LeftBoard.vue
@@ -1,17 +1,33 @@
 <template>
-    <el-row :gutter="2">
-        <el-col :span="12"><el-button></el-button></el-col>
-        <el-col :span="12"><el-button></el-button></el-col>
-        <el-col :span="12"><el-button></el-button></el-col>
-        <el-col :span="12"><el-button></el-button></el-col>
-        <el-col :span="12"><el-button></el-button></el-col>
-        <el-col :span="12"><el-button></el-button></el-col>
+    <el-row>
+        <draggable :list="componentsResourceList" group="test" :animation="400" :sort="true">
+            <el-col :span="10" class="component-item" v-for="item in componentsResourceList" :key="item.key">
+                {{item.name}}
+            </el-col>
+        </draggable>
     </el-row>
 </template>
 
 <script>
+    import draggable from "vuedraggable";
+
     export default {
-        name: "LeftBoard"
+        name: "LeftBoard",
+        components: {
+            draggable
+        },
+        data() {
+            return {
+                componentsResourceList: [
+                    {key: 1, name: "111"},
+                    {key: 2, name: "222"},
+                    {key: 3, name: "333"},
+                    {key: 4, name: "444"},
+                    {key: 5, name: "555"},
+                ],
+            };
+        }
+
     }
 </script>
 
