3a096826e7517d495b96a218ad3064850befe0b1
sundae <958820549@qq.com>
2020-05-25 15:27:10 +0800

    update

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,39 +4,45 @@
             <span>表单生成器</span>
         </el-header>
         <el-main>
-            <div class="left-board">
-                <el-srollbar class="left-scrollbar">
-<!--                    <draggable-->
-<!--                            :list="componentList"-->
-<!--                            :animation="340"-->
-<!--                            :group="{ name: 'testgroup', pull: 'clone', put: false }"-->
-<!--                            :sort="false"-->
-<!--                            :clone="onCloneHandler"-->
-<!--                    >-->
-<!--                        <div-->
-<!--                                class="component-item"-->
-<!--                                v-for="item in componentList"-->
-<!--                                :key="item.key"-->
-<!--                        >{{item.label}}-->
-<!--                        </div>-->
-<!--                    </draggable>-->
-                </el-srollbar>
-            </div>
-            <div class="center-board">
-                <div class="center-scrollbar">
-                    <Main @onSelectComponent="onSelectFormComponent"/>
-                </div>
-            </div>
-            <div class="right-board">
-                <component :is="configView" v-model="config"></component>
-            </div>
+            <div style="height:calc(100vh - 42px);display: inline;background-color: #2c3e50;width: 260px;position: absolute;">111</div>
+            <div style="height:calc(100vh - 42px);display: inline;background-color: #42b983;left:0px; right:0px;margin:0px 260px;position: absolute;">222</div>
+            <div style="height:calc(100vh - 42px);display: inline;background-color: #2c3e50;width: 260px;right:0px;position: absolute;">333</div>
+<!--            <div class="left-board">-->
+<!--                <el-srollbar class="left-scrollbar">-->
+<!--                    <LeftBoard/>-->
+<!--&lt;!&ndash;                    <draggable&ndash;&gt;-->
+<!--&lt;!&ndash;                            :list="componentList"&ndash;&gt;-->
+<!--&lt;!&ndash;                            :animation="340"&ndash;&gt;-->
+<!--&lt;!&ndash;                            :group="{ name: 'testgroup', pull: 'clone', put: false }"&ndash;&gt;-->
+<!--&lt;!&ndash;                            :sort="false"&ndash;&gt;-->
+<!--&lt;!&ndash;                            :clone="onCloneHandler"&ndash;&gt;-->
+<!--&lt;!&ndash;                    >&ndash;&gt;-->
+<!--&lt;!&ndash;                        <div&ndash;&gt;-->
+<!--&lt;!&ndash;                                class="component-item"&ndash;&gt;-->
+<!--&lt;!&ndash;                                v-for="item in componentList"&ndash;&gt;-->
+<!--&lt;!&ndash;                                :key="item.key"&ndash;&gt;-->
+<!--&lt;!&ndash;                        >{{item.label}}&ndash;&gt;-->
+<!--&lt;!&ndash;                        </div>&ndash;&gt;-->
+<!--&lt;!&ndash;                    </draggable>&ndash;&gt;-->
+<!--                </el-srollbar>-->
+<!--            </div>-->
+<!--            <div class="center-board">-->
+<!--                <div class="center-scrollbar">-->
+<!--                    <Main @onSelectComponent="onSelectFormComponent"/>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="right-board">-->
+<!--                <component :is="configView" v-model="config"></component>-->
+<!--            </div>-->
         </el-main>
     </div>
 </template>
 
 <script>
+    // import LeftBoard from "@/views/LeftBoard";
     export default {
-        name: "Index"
+        name: "Index",
+        // components: {LeftBoard},
     }
 </script>
 
---
+++ src/views/LeftBoard.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-row :gutter="2">
+        <el-col :span="12"><el-button></el-button></el-col>
+        <el-col :span="12"><el-button></el-button></el-col>
+        <el-col :span="12"><el-button></el-button></el-col>
+        <el-col :span="12"><el-button></el-button></el-col>
+        <el-col :span="12"><el-button></el-button></el-col>
+        <el-col :span="12"><el-button></el-button></el-col>
+    </el-row>
+</template>
+
+<script>
+    export default {
+        name: "LeftBoard"
+    }
+</script>
+
+<style scoped>
+
+</style>
